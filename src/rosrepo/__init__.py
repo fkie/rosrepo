d194136e80d5044b5c2aa559b647a49dc52c6aa2
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2014-07-04 13:53:34 +0200

    1.0.24

--- src/rosrepo/__init__.py
+++ src/rosrepo/__init__.py
@@ -4,6 +4,6 @@
 __author__ = "Timo Röhling <timo.roehling@fkie.fraunhofer.de>"
 __copyright__ = "Copyright (c) 2013 Fraunhofer FKIE"
 __license__ = "BSD"
-__version__ = "1.0.23"
+__version__ = "1.0.24"
 __all__ = [ "cmd_init", "cmd_checkout", "cmd_list", "cmd_include", "cmd_bash",
             "cmd_exclude", "cmd_build", "cmd_find", "cmd_use", "cmd_uninit" ]
