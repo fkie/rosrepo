5c395d1ec0c293eb1f2957645029f498b2c9a697
Timo Röhling <timo.roehling@gmx.de>
2016-07-02 16:00:04 +0200

    Unit test for NamedTuple

--- test/test_util.py
+++ test/test_util.py
@@ -15,6 +15,9 @@ sys.stderr = sys.stdout
 
 import rosrepo.util as util
 
+class TestTuple(util.NamedTuple):
+    __slots__ = ("first", "second")
+
 class UtilTest(unittest.TestCase):
     def test_path_has_prefix(self):
         self.assertTrue(util.path_has_prefix("abc", "abc"))
@@ -28,3 +31,19 @@ class UtilTest(unittest.TestCase):
         self.assertFalse(util.path_has_prefix("/abc/efg", "/hij/klm"))
         self.assertFalse(util.path_has_prefix("abc/efg", "hij/klm"))
 
+    def test_named_tuple(self):
+        t = TestTuple(1, 2)
+        self.assertEqual(len(t), 2)
+        self.assertEqual(t.first, 1)
+        self.assertEqual(t[0], 1)
+        self.assertEqual(t.second, 2)
+        self.assertEqual(t[1], 2)
+        self.assertEqual(str(t), "TestTuple(first=1, second=2)")
+        self.assertEqual([d for d in t], [1, 2])        
+        t = TestTuple(first=3, second=4)
+        self.assertEqual(t.first, 3)
+        self.assertEqual(t.second, 4)
+        t.first = 5
+        self.assertEqual(t.first, 5)
+        t[1] = 6
+        self.assertEqual(t.second, 6)
