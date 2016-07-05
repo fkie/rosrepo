1708ef09769830ceb6cbc2761f62e0abea4fe460
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2016-07-05 09:31:54 +0200

    Publish under Apache License 2.0

--- src/rosrepo/terminal_color.py
+++ src/rosrepo/terminal_color.py
@@ -5,17 +5,13 @@
 #
 # Author: Timo Röhling
 #
-# Copyright (c) 2016 Fraunhofer FKIE
-#
-#
-
 ##############################################################################
 # This file is originally from catkin tools, and has been modified for
-# ROSREPO. It is reused under the following license:
+# ROSREPO.
 ##############################################################################
-
 #
 # Copyright 2014 Open Source Robotics Foundation, Inc.
+# Copyright 2016 Fraunhofer FKIE
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,7 +24,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
+#
 """
 Module to enable color terminal output
 """
