a5495bbda9ab606325cd345e19a7434931fb6e24
Timo Röhling <timo.roehling@fkie.fraunhofer.de>
2017-06-26 12:04:27 +0200

    Always run rosclipse with the environment from the workspace

--- src/rosrepo/cmd_build.py
+++ src/rosrepo/cmd_build.py
@@ -191,7 +191,8 @@ def run(args):
     rosclipse = find_program("rosclipse")
     use_rosclipse = args.rosclipse or (args.rosclipse is None and config.get("use_rosclipse", True))
     force_rosclipse = args.rosclipse
-    if rosclipse is not None and use_rosclipse:
+    develdir = os.path.join(wsdir, "devel")
+    if rosclipse is not None and use_rosclipse and os.path.isdir(develdir):
         eclipse_ok, _, _ = call_process([rosclipse, "-d"], stdout=PIPE, stderr=PIPE)
         if eclipse_ok == 0:
             workload = []
@@ -201,7 +202,7 @@ def run(args):
                     p_time = max(getmtime(os.path.join(pkgdir, "CMakeLists.txt")), getmtime(os.path.join(pkgdir, "package.xml")))
                     e_time = getmtime(os.path.join(pkgdir, ".project"))
                     if e_time < p_time or force_rosclipse:
-                        workload.append((rosclipse, name, args.dry_run))
+                        workload.append((develdir, rosclipse, name, args.dry_run))
             run_multiprocess_workers(update_rosclipse, workload, jobs=jobs)
     if os.path.isdir(os.path.join(wsdir, "devel", "bin")) and not env_path_list_contains("PATH", os.path.join(wsdir, "devel", "bin")):
         warning("%s is not in PATH\n" % os.path.join(wsdir, "devel", "bin"))
@@ -217,7 +218,7 @@ def run(args):
 
 
 def update_rosclipse(part):
-    rosclipse, name, dry_run = part[0], part[1], part[2]
+    develdir, rosclipse, name, dry_run = part[0], part[1], part[2], part[3]
     msg("@{cf}Updating rosclipse project files@|: %s\n" % name)
     if not dry_run:
-        call_process([rosclipse, name])
+        call_process([os.path.join(develdir, "env.sh"), rosclipse, name])
